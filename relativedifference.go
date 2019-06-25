8e2311570ded6ba13a5c760e030ed8ffcc558132
maseology <mason@riffle.ca>
2019-06-24 21:45:21 -0400

    division by zero fix relative difference

--- relativedifference.go
+++ relativedifference.go
@@ -2,5 +2,8 @@ package mmaths
 
 // RelativeDifference returns the relative difference between to values
 func RelativeDifference(f0, f1 float64) float64 {
+	if f0 == 0. {
+		return f1 - f0
+	}
 	return (f1 - f0) / f0
 }
