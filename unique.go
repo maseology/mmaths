ac181006401bb00d5e31703afb705059cd835fe0
maseology <maseology@pm.me>
2022-11-21 11:48:09 -0500

    edited unique

--- unique.go
+++ unique.go
@@ -3,13 +3,15 @@ package mmaths
 // UniqueInts returns a unique subset of the int slice provided.
 // from: https://kylewbanks.com/blog/creating-unique-slices-in-go
 func UniqueInts(input []int) []int {
-	u := make([]int, 0, len(input))
-	m := make(map[int]bool)
+	m := make(map[int]int)
 	for _, val := range input {
 		if _, ok := m[val]; !ok {
-			m[val] = true
-			u = append(u, val)
+			m[val]++
 		}
+	}
+	u := make([]int, 0, len(m))
+	for c := range m {
+		u = append(u, c)
 	}
 	return u
 }
