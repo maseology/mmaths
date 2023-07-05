ec98145a1a6056e7213c79af8acb52807c591707
maseology <maseology@pm.me>
2023-07-05 09:38:24 -0400

    fixed distinct that maintains order

--- slice/distinct.go
+++ slice/distinct.go
@@ -2,17 +2,28 @@ package slice
 
 // Distinct returns a unique slice
 func Distinct(ints []int) []int {
-	k := make(map[int]bool)
-	for _, i := range ints {
-		if _, ok := k[i]; !ok {
-			k[i] = true
+	eval := make(map[int]bool)
+	l := []int{}
+	for _, v := range ints {
+		if eval[v] {
+			// do nothing
+		} else {
+			eval[v] = true
+			l = append(l, v)
 		}
 	}
-	l, ii := make([]int, len(k)), 0
-	for i := range k {
-		l[ii] = i
-		ii++
-	}
+	// // does not preserve order
+	// k := make(map[int]bool)
+	// for _, i := range ints {
+	// 	if _, ok := k[i]; !ok {
+	// 		k[i] = true
+	// 	}
+	// }
+	// l, ii := make([]int, len(k)), 0
+	// for i := range k {
+	// 	l[ii] = i
+	// 	ii++
+	// }
 	return l
 }
 
